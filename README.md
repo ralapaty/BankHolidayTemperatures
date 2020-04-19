54406a611f52a544d83345679cc8886d83c8cd9d
Rajendra Alapaty <rajendra.alapaty@gmail.com>
2020-04-19 14:39:54 +0100

    Refactored RestController to use Spring ResponseEntity

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BankHolidayTemperatures
-Restful API to return temperatures for a UK city on bank holidays. 
+Restful API to return temperatures for a UK city on bank holidays.
+
+#Software Dependencies used
+JDK 1.8.0_241
+Maven 3.6.3_1
--- src/main/java/me/alapaty/bankholidays/Main.java
+++ src/main/java/me/alapaty/bankholidays/Main.java
@@ -6,12 +6,16 @@ import java.util.List;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import me.alapaty.bankholidays.beans.ErrorResponse;
 import me.alapaty.bankholidays.exceptions.CityNotFoundException;
 import me.alapaty.bankholidays.exceptions.WeatherDataNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,22 +25,24 @@ import me.alapaty.bankholidays.beans.Holiday;
 
 @RestController
 public class Main {
-	private Logger log = LoggerFactory.getLogger(Main.class);
-	@Autowired
-	private BankHolidayService bankHolidayService;
+    private Logger log = LoggerFactory.getLogger(Main.class);
+    @Autowired
+    private BankHolidayService bankHolidayService;
 
-	@GetMapping(path = "/{city}/temps")
-	public Response findBankHolidayTemperaturesByCity(@PathVariable String city, @QueryParam(value = "startDate") String startDate, @QueryParam(value = "endDate") String endDate) {
-		List<Holiday> holidays = null;
-		ErrorResponse errorResponse = new ErrorResponse();
-		try {
-			holidays = bankHolidayService.retrieveTemperaturesForBankHolidayForCityBetweenDates(city, startDate, endDate);
-		} catch (CityNotFoundException | WeatherDataNotFound e) {
-			errorResponse.setErrorMessage(e.getMessage());
-			errorResponse.setStatusCode(Response.Status.BAD_REQUEST);
-			return Response.status(Response.Status.BAD_REQUEST).entity(errorResponse).build();
-		}
-		log.info("Bank Holiday Temperatures for city {} are {}", city, holidays);
-		return  Response.status(Response.Status.OK).entity(holidays).build();
-	}
+    @GetMapping(path = "/{city}/temps")
+    @ApiOperation(value = "Return min, max temperatures for a UK city on bank holidays", notes = "Return min, max temperatures for a UK city on bank holidays", response = Holiday[].class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Retrieved min, max temperatures for a UK city on bank holidays", response = Holiday[].class), @ApiResponse(code = 400, message = "Either invalid city entered or no data found for city, and date", response = ErrorResponse.class)})
+    public ResponseEntity findBankHolidayTemperaturesByCity(@PathVariable String city, @QueryParam(value = "startDate") String startDate, @QueryParam(value = "endDate") String endDate) {
+        List<Holiday> holidays = null;
+        ErrorResponse errorResponse = new ErrorResponse();
+        try {
+            holidays = bankHolidayService.retrieveTemperaturesForBankHolidayForCityBetweenDates(city, startDate, endDate);
+        } catch (CityNotFoundException | WeatherDataNotFound e) {
+            errorResponse.setErrorMessage(e.getMessage());
+            errorResponse.setStatusCode(Response.Status.BAD_REQUEST);
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+        log.info("Bank Holiday Temperatures for city {} are {}", city, holidays);
+        return ResponseEntity.ok(holidays);
+    }
 }
